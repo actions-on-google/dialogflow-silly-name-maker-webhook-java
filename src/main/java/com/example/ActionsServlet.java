2c24f438e30e33058049923a6a24b1a871286986
Aza Tulepbergenov <atulep@google.com>
2018-11-15 11:55:13 -0800

    Initial code files for the sample.
    Bug: 117573221
    Change-Id: Iacb5f55e2111de62a2054535fbf2dad340142e70

---
+++ src/main/java/com/example/ActionsServlet.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import com.google.actions.api.App;
+import java.io.IOException;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Handles request received via HTTP POST and delegates it to your Actions app. See: [Request
+ * handling in Google App
+ * Engine](https://cloud.google.com/appengine/docs/standard/java/how-requests-are-handled).
+ */
+@WebServlet(name = "actions", value = "/")
+public class ActionsServlet extends HttpServlet {
+  private static final Logger LOG = LoggerFactory.getLogger(SillyNameMakerApp.class);
+  private final App actionsApp = new SillyNameMakerApp();
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    String body = req.getReader().lines().collect(Collectors.joining());
+    LOG.info("doPost, body = {}", body);
+    actionsApp
+        .handleRequest(body, null)
+        .thenAccept(
+            (Consumer<String>)
+                jsonResponse -> {
+                  LOG.info("Generated json = {}", jsonResponse);
+                  res.setContentType("application/json");
+                  writeResponse(res, jsonResponse);
+                })
+        .exceptionally(
+            (throwable -> {
+              LOG.error("Error in App.handleRequest ", throwable);
+              writeResponse(res, "Error handling the intent - " + throwable);
+              return null;
+            }));
+  }
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/plain");
+    response
+        .getWriter()
+        .println(
+            "ActionsServlet is listening but requires valid POST request to respond with Action response.");
+  }
+
+  private void writeResponse(HttpServletResponse res, String asJson) {
+    try {
+      res.getWriter().write(asJson);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ src/main/java/com/example/SillyNameMakerApp.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import com.google.actions.api.ActionRequest;
+import com.google.actions.api.ActionResponse;
+import com.google.actions.api.DialogflowApp;
+import com.google.actions.api.ForIntent;
+import com.google.actions.api.response.ResponseBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SillyNameMakerApp extends DialogflowApp {
+
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(SillyNameMakerApp.class);
+
+  // Note: Do not store any state as an instance variable.
+  // It is ok to have final variables where the variable is assigned a value in
+  // the constructor but remains unchanged. This is required to ensure thread-
+  // safety as the entry point (ActionServlet/ActionsAWSHandler) instances may
+  // be reused by the server.
+
+  @ForIntent("make_name")
+  public ActionResponse make_name(ActionRequest request) {
+    LOGGER.info("make_name start.");
+    String response = "Alright, your silly name is "
+            + request.getParameter("color") +  " "  + request.getParameter("number") + "! "
+            + "I hope you like it. See you next time.";
+    ResponseBuilder responseBuilder = getResponseBuilder(request).add(response).endConversation();
+    ActionResponse actionResponse = responseBuilder.build();
+    LOGGER.info(actionResponse.toString());
+    LOGGER.info("make_name end.");
+    return actionResponse;
+  }
+}
